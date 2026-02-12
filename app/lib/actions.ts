0e724d2d4b5e94937c3a96bb3d87548d101d5669
Abdul Rahman <abdulrahman161004@gmail.com>
2026-02-12 20:21:24 +0530

    feat: implement invoice creation using Next.js Server Actions
    Technical Implementation Details:
    1. Server-Side Logic: Created 'createInvoice' Server Action to handle form submissions securely on the server, bypassing the need for manual API endpoints.
    2. Data Extraction & Validation: Utilized the 'formData' API to extract user inputs and implemented Zod schema validation to ensure data integrity before database insertion.
    3. Database Integration: Orchestrated the asynchronous insertion of validated invoice data into the PostgreSQL/Supabase instance.
    4. Cache Invalidation: Applied 'revalidatePath' to clear the client-side cache for the '/dashboard/invoices' route, ensuring the UI reflects the latest data immediately.
    5. UX Flow: Implemented a seamless 'redirect' to return users to the main invoices table upon successful creation.

---
+++ app/lib/actions.ts
@@ -0,0 +1,36 @@
+"use server";
+
+import { z } from "zod";
+import { revalidatePath } from "next/cache";
+import postgres from "postgres";
+import { redirect } from "next/navigation";
+
+const sql = postgres(process.env.POSTGRES_URL!, { ssl: "require" });
+
+const FormSchema = z.object({
+    id: z.string(),
+    customerId: z.string(),
+    amount: z.coerce.number(), // The amount field is specifically set to coerce (change) from a string to a number while also validating its type.
+    status: z.enum(["pending", "paid"]),
+    date: z.string(),
+});
+
+const CreateInvoice = FormSchema.omit({ id: true, date: true });
+
+export async function createInvoice(formData: FormData) {
+    const { customerId, amount, status } = CreateInvoice.parse({
+        customerId: formData.get("customerId"),
+        amount: formData.get("amount"),
+        status: formData.get("status"),
+    });
+    const amountInCents = amount * 100; // convert the amount into cents:
+    const date = new Date().toISOString().split("T")[0]; //Finally, let's create a new date with the format "YYYY-MM-DD" for the invoice's creation date:
+
+    await sql`
+    INSERT INTO invoices (customer_id, amount, status, date)
+    VALUES (${customerId}, ${amountInCents}, ${status}, ${date})
+  `;
+
+    revalidatePath("/dashboard/invoices");
+    redirect("/dashboard/invoices");
+}
